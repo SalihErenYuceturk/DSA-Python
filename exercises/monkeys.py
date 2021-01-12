7128ddbe144de28b79685a6975684401f7aee061
Salih Eren Yuceturk <s.e.yuceturk@gmail.com>
2021-01-12 20:59:34 +0100

    Implemented compare_string().

--- exercises/monkeys.py
+++ exercises/monkeys.py
@@ -7,10 +7,10 @@ Write 3 functions.
 1. Generates a string that is 28 characters long by choosing random letters from the 26 letters in the alphabet plus
 the space.
 
-2. Scores each generated string by comparing the randomly generated string to the string.
+2. Scores each generated string by comparing the randomly generated string to the goal string.
 
 3. Repeatedly calls generate and score, then if 100% of the letters are correct it is done. If the letters are not
-correct then it will generate a whole new string.This  function should print out the best string generated so
+correct then it will generate a whole new string.This function should print out the best string generated so
 far and its score every 1000 tries.
 """
 
@@ -25,5 +25,15 @@ def generate_string():
     return output
 
 
-print(generate_string())
+def compare_string(string_in):
+    goal_string = "methinks it is like a weasel"
+    index = 0
+    correct_characters = 0
+    while index != 28:
+        if string_in[index] == goal_string[index]:
+            correct_characters += 1
+        index += 1
+    return correct_characters/28  # ratio of similarity between string_in and goal_string
+
+
 
