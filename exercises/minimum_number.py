86d2b1024d17a8c32eee9b58a85bc147b2154088
Salih Eren Yuceturk <s.e.yuceturk@gmail.com>
2021-01-13 15:26:26 +0100

    Implemented method minimum_linear(list_in).

--- exercises/minimum_number.py
+++ exercises/minimum_number.py
@@ -14,3 +14,11 @@ def minimum_squared(list_in):
             if search_number > scan_number and scan_number < minimum:
                 minimum = scan_number
     return minimum
+
+
+def minimum_linear(list_in):
+    minimum = list_in[0]
+    for number in list_in:
+        if number < minimum:
+            minimum = number
+    return minimum
