ae01fcd77e8356f13bccd47432d72d6df5716abb
Salih Eren Yuceturk <s.e.yuceturk@gmail.com>
2021-01-13 15:24:12 +0100

    Implemented method minimum_squared(list_in).

---
+++ exercises/minimum_number.py
@@ -0,0 +1,16 @@
+"""
+Write two Python functions to find the minimum number in a list.
+
+1. Compare each number to every other number on the list. O(n^2).
+
+2. Find the minimum value with linear complexity. O(n).
+"""
+
+
+def minimum_squared(list_in):
+    minimum = list_in[0]
+    for search_number in list_in:
+        for scan_number in list_in:
+            if search_number > scan_number and scan_number < minimum:
+                minimum = scan_number
+    return minimum
