44fc3a6b23514212e3d395fc3fa2f18c6cca9f38
Salih Eren Yuceturk <s.e.yuceturk@gmail.com>
2021-01-15 20:37:20 +0100

    Implemented general_balanced_brackets.

---
+++ exercises/3_basic_data_structures/general_balanced_brackets.py
@@ -0,0 +1,22 @@
+from data_structures.linear_data_structures import Stack
+
+
+def check_balance(string_in):
+    balance = Stack()
+    open_brackets = "([{"
+    close_brackets = ")]}"
+    for element in string_in:
+        if element in ")]}" and balance.is_empty():
+            return False
+        if element in "([{":
+            balance.push(element)
+        if element in ")]}":
+            top = balance.pop()
+            if open_brackets.index(top) != close_brackets.index(element):
+                return False
+    return True
+
+
+# print(check_balance('{({([][])}())}'))
+# print(check_balance('[{(})]'))
+
