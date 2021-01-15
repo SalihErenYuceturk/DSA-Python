b7f19ff4fb2aab0c933daafd3a6d27d2069b7e3a
Salih Eren Yuceturk <s.e.yuceturk@gmail.com>
2021-01-15 15:46:50 +0100

    Implemented check_balance(string_in).

---
+++ exercises/3_basic_data_structures/simple_balanced_brackets.py
@@ -0,0 +1,21 @@
+from data_structures.linear_data_structures import Stack
+
+"""
+Starting with an empty stack, process the parenthesis strings from left to right. If an element is "(", push it in 
+the stack. If ana element is ")" pop the stack. If at the end the stack is not empty, the brackets are not balanced.
+"""
+
+
+def check_balance(string_in):
+    balance = Stack()
+    for element in string_in:
+        if element == "(":
+            balance.push("(")
+        if element == ")" and not balance.is_empty():
+            balance.pop()
+        if element == ")" and balance.is_empty():
+            return False
+    return balance.is_empty()
+
+
+# print(check_balance(")(()"))
