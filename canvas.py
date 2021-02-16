6183713311892c561e6844082c1444df1744d90e
Salih Eren Yuceturk <s.e.yuceturk@gmail.com>
2021-02-16 13:19:52 +0100

    Messing around with recursion.

--- canvas.py
+++ canvas.py
@@ -1,12 +1,44 @@
-
-sum_list = [2, 4, 0, 8, 10]
-
-
-def recursive_sum(list_in):
-    if len(list_in) == 1:
-        return list_in[0]
-    else:
-        return list_in[0] + recursive_sum(list_in[1:])
+import turtle
 
 
-print(recursive_sum(sum_list))
+def drawTriangle(points,color,myTurtle):
+    myTurtle.fillcolor(color)
+    myTurtle.up()
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.down()
+    myTurtle.begin_fill()
+    myTurtle.goto(points[1][0],points[1][1])
+    myTurtle.goto(points[2][0],points[2][1])
+    myTurtle.goto(points[0][0],points[0][1])
+    myTurtle.end_fill()
+
+def getMid(p1,p2):
+    return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
+
+def sierpinski(points,degree,myTurtle):
+    colormap = ['blue','red','green','white','yellow',
+                'violet','orange']
+    drawTriangle(points,colormap[degree],myTurtle)
+    if degree > 0:
+        sierpinski([points[0],
+                        getMid(points[0], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[1],
+                        getMid(points[0], points[1]),
+                        getMid(points[1], points[2])],
+                   degree-1, myTurtle)
+        sierpinski([points[2],
+                        getMid(points[2], points[1]),
+                        getMid(points[0], points[2])],
+                   degree-1, myTurtle)
+
+def main():
+   myTurtle = turtle.Turtle()
+   myWin = turtle.Screen()
+   myPoints = [[-100,-50],[0,100],[100,-50]]
+   sierpinski(myPoints, 4, myTurtle)
+   myWin.exitonclick()
+
+main()
+
